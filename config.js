cce21adc5e402f88065606d8e9769ccfa0b8b159
PoojaRangasheshan <94818384+PoojaRangasheshan@users.noreply.github.com>
2021-12-14 15:18:12 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDzkJYd2PRbnTVcbD1cOUqCXRjfCroq8ko",
+  authDomain: "storytelling-34d2a.firebaseapp.com",
+  databaseURL: "https://storytelling-34d2a-default-rtdb.firebaseio.com",
+  projectId: "storytelling-34d2a",
+  storageBucket: "storytelling-34d2a.appspot.com",
+  messagingSenderId: "265324046330",
+  appId: "1:265324046330:web:ef9f620d727c605f4a2643"
+};
